666b2a1d62c8d9ab73446418092fbaa0d9855f6d
realsahabia <yakubukarim12@gmail.com>
2023-09-05 00:01:31 +0000

    update

--- src/conponents/Home.js
+++ src/conponents/Home.js
@@ -7,13 +7,13 @@ const Home = () => {
     <div name="home" className='sm:bg-slate-900 w-full h-screen flex justify-center items-center'>
       
 {/* container */}
-<div className='max-w-[1000px] mx-auto sm:p-8 h-[90vh] grid grid-cols-10 sm:grid-cols-6 items-center border-2 border-pink-600'>
+<div className='max-w-[1000px] mx-auto sm:p-8 grid grid-cols-10 sm:grid-cols-6 items-center'>
   
-  {/* Content div (60%) */}
-  <div className='col-span-10 sm:col-span-3 border-2 border-pink-600 '>
-    <div className='h-full mr-1 flex flex-col justify-center mt-[250px] bg-[#0000007e] sm:bg-[#0000] sm:col-span-5 p-2'>
+  {/* Content div */}
+  <div className='col-span-10 sm:col-span-3 sm:h-[70vh]'>
+    <div className='h-full mr-1 flex flex-col justify-center mt-[250px] sm:mt-0 bg-[#0000007e] sm:bg-[#0000] sm:col-span-5 p-2'>
       <p className='text-[#FF6100]'>Hi, my name is</p>
-      <h1 className='text-5xl sm:text-6xl font-bold text-[#ccd6f6]'>Sahabia Yakubu</h1>
+      <h1 className='text-4xl sm:text-5xl font-bold text-[#ccd6f6]'>SAHABIA YAKUBU</h1>
       <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>I am a Web Developer</h2>
       <p className='py-4 text-[#8892b0] max-w-[700px]'>I’m a web developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building responsive frontend web applications.</p>
       <div>
@@ -23,17 +23,16 @@ const Home = () => {
           </span>
         </button>
       </div>
-       </div>
-       </div>
+    </div>
+  </div>
 
         {/* Image div (40%) */}
-        <div className='sm:col-span-3 flex h-[100vh] justify-center items-center sm:ml-5 grid-pic bg-[#8892b0] sm:rounded-tl-[70px] sm:rounded-tr-[70px]'>
+        <div className='sm:col-span-3 flex h-full sm:h-[70vh] justify-center items-center sm:ml-5 grid-pic bg-[#8892b0] sm:rounded-tl-[70px] sm:rounded-tr-[70px]'>
           <img src={sahabia} alt='./' className='w-full h-full object-cover sm:flex-col' />
         </div>
-      </div>
+</div>
 
-          
-  </div>
+</div>
   )
 }
 
