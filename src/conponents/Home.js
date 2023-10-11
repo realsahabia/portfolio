86cb40342b07c3d63e85d58a8f84a5d3d834e805
realsahabia <yakubukarim12@gmail.com>
2023-10-11 12:40:31 +0000

    update

--- src/conponents/Home.js
+++ src/conponents/Home.js
@@ -11,11 +11,12 @@ const Home = () => {
   
   {/* Content div */}
   <div className='col-span-10 sm:col-span-3 sm:h-[70vh]'>
-    <div className='h-full mr-1 flex flex-col justify-center mt-[250px] sm:mt-0 bg-[#0000007e] sm:bg-[#0000] sm:col-span-5 p-2'>
+    <div className='h-[100vh] mr-1 flex flex-col justify-center sm:mt-0 bg-[#0000007e] sm:bg-[#0000] sm:col-span-5 
+    p-2 pr-4 pt-24 sm:pr-0 sm:pt-0'>
       <p className='text-[#FF6100]'>Hi, my name is</p>
-      <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>Sahabia Yakubu</h1>
-      <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>I am a Web Developer</h2>
-      <p className='py-4 text-[#8892b0] max-w-[700px]'>I’m a web developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building responsive frontend websites.</p>
+      <h1 className='text-4xl sm:text-6xl font-bold text-[#ffffff]'>Sahabia Yakubu</h1>
+      <h2 className='text-4xl sm:text-6xl font-bold text-[#ccc]'>I am a Web Developer</h2>
+      <p className='py-4 text-[#ffffff] max-w-[700px]'>I’m a web developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building responsive frontend websites.</p>
       <div>
         <button className='text-white border-2 px-6 py-3 my-2 group flex items-center hover:bg-[#FF6100] hover:border-[#FF6100]'>View Work 
           <span className='group-hover:rotate-90'>
